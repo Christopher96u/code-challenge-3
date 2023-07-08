05a661485a0cd1d23c22b0e59471c0f98192f7ce
Christopher <christophermenendezmorales@gmail.com>
2023-07-08 23:16:54 +1000

    feat: improve create-transaction.dto

--- fx-backend/src/transactions/dto/create-transaction.dto.ts
+++ fx-backend/src/transactions/dto/create-transaction.dto.ts
@@ -1,4 +1,10 @@
-import { IsNotEmpty, IsString, Length } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Length,
+} from 'class-validator';
 
 export class CreateTransactionDto {
   @IsString()
@@ -6,8 +12,28 @@ export class CreateTransactionDto {
   @Length(3)
   readonly currencyFrom: string;
 
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  readonly sourceAmount: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  readonly targetAmount: number;
+
   @IsString()
   @IsNotEmpty()
   @Length(3)
   readonly currencyTo: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
+  readonly fee: number;
+
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
+  readonly marketRate: number;
 }
